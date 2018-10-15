78dcb1512e9cbb987ae7b2c72c96010b377a8464
jnf <jnf@has-many.com>
2018-10-14 18:33:02 -0700

    using the existing objects is enough to have a rudimentary chat client. This is just enough code for me to verify that information is traveling up and down the websocket as intended

---
+++ client/index.js
@@ -0,0 +1,41 @@
+import SocketManager from "./lib/socketManager"
+import User from "./lib/user"
+
+document.addEventListener("DOMContentLoaded", () => {
+  const user = new User()
+  const talker = document.getElementById("talker")
+  const messages = document.getElementById("messages")
+  const message = document.getElementById("message")
+  const socketManager = new SocketManager({
+    "chat:app:register": () => {
+      socketManager.emit("chat:client:register", {id: user.id})
+    },
+    "chat:message:receive": (response) => {
+        const content = document.createElement("li")
+        content.innerText = response
+        messages.appendChild(content)
+    },
+    "chat:user:connected": (name) => {
+      const content = document.createElement("li")
+      content.classList.add("system-message")
+      content.innerText = `${name} connected`
+      messages.appendChild(content)
+    },
+    "chat:client:disconnected": (response) => {
+      const content = document.createElement("li")
+      content.classList.add("system-message")
+      content.innertext = `${response} disconnected`
+      messages.appendChild(content)
+    },
+  })
+
+  talker.addEventListener("submit", (event) => {
+    event.preventDefault()
+    socketManager.emit('chat:message:send', {
+      message: message.value,
+      id: user.id
+    })
+
+    message.value = ""
+  })
+})
