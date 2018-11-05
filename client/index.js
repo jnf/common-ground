35a703365a00fdd1b26dcdf71d1d113a582c70ae
jnf <jnf@has-many.com>
2018-11-04 16:22:56 -0800

    removes socket handlers for chat client messages, leaving a bit of documentation on how to write them because I will forget sometimes

--- client/index.js
+++ client/index.js
@@ -3,36 +3,13 @@ import User from "./lib/user"
 
 document.addEventListener("DOMContentLoaded", () => {
   const user = new User()
-  const socketManager = new SocketManager({
-    "chat:app:register": () => {
-      socketManager.emit("chat:client:register", {id: user.id})
-    },
-    "chat:message:receive": (response) => {
-        const content = document.createElement("li")
-        content.innerText = response
-        messages.appendChild(content)
-    },
-    "chat:user:connected": (name) => {
-      const content = document.createElement("li")
-      content.classList.add("system-message")
-      content.innerText = `${name} connected`
-      messages.appendChild(content)
-    },
-    "chat:client:disconnected": (response) => {
-      const content = document.createElement("li")
-      content.classList.add("system-message")
-      content.innertext = `${response} disconnected`
-      messages.appendChild(content)
-    },
-  })
+  const appMessages = document.getElementById("app-messages")
 
-  talker.addEventListener("submit", (event) => {
-    event.preventDefault()
-    socketManager.emit('chat:message:send', {
-      message: message.value,
-      id: user.id
-    })
-
-    message.value = ""
-  })
+  // here's what listeners should look like:
+  // {
+      // "chat:app:register": () => {
+      //   socketManager.emit("chat:client:register", {id: user.id})
+      // }
+  // }
+  const socketManager = new SocketManager({})
 })
