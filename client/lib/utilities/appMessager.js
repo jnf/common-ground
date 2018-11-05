f4b415e1e0fdf903d926514d81d79a4b459190ed
jnf <jnf@has-many.com>
2018-11-04 17:52:52 -0800

    creates a standalone utility for inserting app messages (used mostly for debugging) into a high-visibility space in the client

---
+++ client/lib/utilities/appMessager.js
@@ -0,0 +1,25 @@
+const message_from = ({message="New app message!", data={}}) => {
+  const header = document.createElement("h3")
+  header.innerText = message
+
+  const dataBlock = document.createElement("ul")
+  Object.entries(data).reduce((ol, [key, value]) => {
+    const li = document.createElement("li")
+    li.innerText = `${key}: ${value}`
+    ol.appendChild(li)
+
+    return ol
+  }, dataBlock)
+
+  const div = document.createElement("div")
+  div.appendChild(header)
+     .appendChild(dataBlock)
+
+  return div
+}
+
+const AppMessager = (element, data) => {
+  element.appendChild(message_from(data))
+}
+
+export default AppMessager
