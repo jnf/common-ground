ffd0830984ba54643a98e5ecf7a1cb1b11679142
jnf <jnf@has-many.com>
2019-02-25 14:35:45 +0000

    adds a registration event for controllers

--- client/control.js
+++ client/control.js
@@ -1,3 +1,15 @@
+import SocketManager from "./lib/socketManager"
+import User from "./lib/user"
+
 document.addEventListener("DOMContentLoaded", () => {
   console.log("hi ❤️")
+
+  const controller = new User({ control: true })
+
+  const socketManager = new SocketManager({
+    "app::register": () => {
+      console.log(`registering controller with ${controller.id}!`)
+      socketManager.emit("control::register", { id: controller.id })
+    }
+  })
 })
