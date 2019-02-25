0e856f010623157157a5a9ba3cd9fa03dc862e73
jnf <jnf@has-many.com>
2019-02-25 14:35:13 +0000

    teaches the client user object to distinguish and identify controllers from users

--- client/lib/user.js
+++ client/lib/user.js
@@ -3,14 +3,19 @@ import StorageManager from "./storageManager"
 
 const map = new WeakMap()
 export default class User {
-  constructor () {
+  constructor (opts) {
+    const config = {
+      control: false,
+      ...opts
+    }
     const storageManager = new StorageManager()
     const id = storageManager.has("id")
       ? storageManager.get("id")
       : storageManager.set("id", uuid())
 
-    map.set(this, { id })
+    map.set(this, { id, ...config })
   }
 
   get id () { return map.get(this).id }
+  get canControl () { return map.get(this).control }
 }
