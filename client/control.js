221cf5b7d0ecdce7f1d3ca6b55bb12026ae7ada1
jnf <jnf@has-many.com>
2019-03-02 16:07:39 +0000

    wow this refactor really cleaned up the controller js loader

--- client/control.js
+++ client/control.js
@@ -1,17 +1,6 @@
-import SocketManager from "./lib/socketManager"
-import User from "./lib/user"
+import ControllerHandler from "./lib/socketHandlers/controller"
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("hi ❤️")
-
-  const controller = new User({ control: true })
-
-  const socketManager = new SocketManager({
-    "message": (message, payload) => console.log("generic message", message, payload),
-    "app::controllerMessage": data => console.log(data),
-    "app::register": () => {
-      console.log(`registering controller with ${controller.id}!`)
-      socketManager.send("control::register", { id: controller.id })
-    }
-  })
+  new ControllerHandler()
 })
