1b6f52b3aeb4bf2780cae6498387ceeb9bc4d048
jnf <jnf@has-many.com>
2019-03-02 16:24:27 +0000

    adds a client socket handler for participants and moves the functionality out of the dom loader into its instance methods

--- client/index.js
+++ client/index.js
@@ -1,20 +1,6 @@
-import SocketManager from "./lib/socketManager"
-import appMessager from "./lib/utilities/appMessager"
-import User from "./lib/user"
+import ParticipantHandler from "./lib/socketHandlers/participant"
 
 document.addEventListener("DOMContentLoaded", () => {
-  const user = new User()
   const appMessages = document.getElementById("app-messages")
-  const socketManager = new SocketManager({
-    "message": (message, data) => appMessager(appMessages, data),
-    "app::register": () => {
-      appMessager(appMessages, {
-        message: `registering with ${user.id}!`,
-        ok: true,
-        data: {}
-      })
-      socketManager.send("client::register", { id: user.id })
-    }
-  })
-
+  new ParticipantHandler({ appMessages })
 })
