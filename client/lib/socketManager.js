13ff48b7126708bf1df639ee2418e0045b33684c
jnf <jnf@has-many.com>
2019-03-02 15:37:48 +0000

    refactors socketManager to create instance methods in its constructor. I don't think I like this pattern, so it probably won't survive long. Probably gonna make it look and act like the message handlers on the server side, since it's doing very similar kind of work here

--- client/lib/socketManager.js
+++ client/lib/socketManager.js
@@ -2,21 +2,33 @@ import * as io from "socket.io-client"
 
 const map = new WeakMap()
 class SocketManager {
-  constructor (callbacks={}) {
-    // establish initial state
-    map.set(this, { callbacks, socket: io() })
+  constructor (messages={}) {
+    // get us a socket connection, and hold onto it
+    map.set(this, { socket: io() })
 
-    // set up listeners for provided callbacks
-    Object.entries(callbacks).forEach(([key, callback]) => this.socket.on(key, callback))
+    // set up messages as instance methods
+    Object.entries(messages).forEach(([message, method]) => this[message] = method)
+
+    // set incoming messages to delegate to the defined instance methods
+    this.state.socket.on("message", this.on.bind(this))
   }
 
-  // use the open connection to send a message to the server
+  on (message, payload={}) {
+    console.log(`received message: "${message}" with payload ${payload}`)
+    try {
+      this[message](payload)
+    } catch (error) {
+      console.error("😵", error)
+      console.table(payload)
+    }
+  }
+
   send (message, data={}) {
     this.socket.send(message, data)
   }
 
-  get callbacks () { return map.get(this).callbacks }
-  get socket () { return map.get(this).socket }
+  get state () { return { ...map.get(this) } } // doesn't break deep references 😒
+  get socket () { return this.state.socket }
 }
 
 export default SocketManager
